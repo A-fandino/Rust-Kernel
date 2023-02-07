7c2a7d875822e921b258b9a534409c237e1b0038
A-fandino <arnauf8@gmail.com>
2023-02-07 23:33:36 +0100

    Support VGA_BUFFER Writting

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 #![no_std] // Don't link STD library
 #![no_main] // Disable all Rust-Level entry points
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-
+mod vga_buffer;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle] // Won't mangle name of the function (The name of the fn will be _start and will not be converted to anything else)
 pub extern "C" fn _start() -> ! {
-    // ENTRY POINT
-    let vga_buffer = 0xb8000 as *mut u8;
-    
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            let position = i as isize * 2;
-            *vga_buffer.offset(position) = byte;
-            *vga_buffer.offset(position + 1) = 0xa;
-        }
-    }
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello from _start\n\n").unwrap();
+    // write!(vga_buffer::WRITER.lock(), "You are the Kernel Run #{} of this year. You got rewarded with {} penies.", 42, 2).unwrap();
 
+    // This is a reimplementation of println! (src/vga_buffer.rs)
+    println!("You are the Kernel Run #{} of this year. You got rewarded with 2 penies.", 42);
+    panic!("AH! I panicked...");
     loop {}
 }
+
+// let vga_buffer = 0xb8000 as *mut u8;
+
+// for (i, &byte) in HELLO.iter().enumerate() {
+//     unsafe {
+//         let position = i as isize * 2;
+//         *vga_buffer.offset(position) = byte;
+//         *vga_buffer.offset(position + 1) = 0xa;
+//     }
+// }
