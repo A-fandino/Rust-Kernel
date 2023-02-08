2939f6fe3ee856534c9de7edc4cd3707f54a6989
Arnau Fandiño Bueno <arnau.fandino@011h.com>
2023-02-08 18:55:49 +0100

    Add Integration Testing support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 #![no_std] // Don't link STD library
 #![no_main] // Disable all Rust-Level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
-use core::panic::PanicInfo;
 
 mod vga_buffer;
 mod serial;
 
+use core::panic::PanicInfo;
+// use rust_os::println;
+
+#[no_mangle] // Won't mangle name of the function (The name of the fn will be _start and will not be converted to anything else)
+pub extern "C" fn _start() -> ! {
+    println!("You are the Kernel Run #{} of this year. You got rewarded with 2 penies.", 42);
+    
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -19,58 +31,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-
-
-#[no_mangle] // Won't mangle name of the function (The name of the fn will be _start and will not be converted to anything else)
-pub extern "C" fn _start() -> ! {
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello from _start\n\n").unwrap();
-    // write!(vga_buffer::WRITER.lock(), "You are the Kernel Run #{} of this year. You got rewarded with {} penies.", 42, 2).unwrap();
-
-    // This is a reimplementation of println! (src/vga_buffer.rs)
-    println!("You are the Kernel Run #{} of this year. You got rewarded with 2 penies.", 42);
-    // panic!("AH! I panicked...");
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_test() {
-    serial_print!("First Kernel test: ");
-    assert_eq!(1,1);
-    serial_println!("[ok]")
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4); //The 'iobase' that we specified in config.toml
-        port.write(exit_code as u32)
-    }
+    rust_os::test_panic_handler(info)
 }
